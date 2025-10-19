7c569ac46f54b65c32e0c2350f071bd1370e2626
flare <78750206+fflvm1@users.noreply.github.com>
2025-10-19 14:35:53 +0300

    Back to PCs!
    - Ported the updated about menu to the PC version of Qlackjack
    - The status bar is no longer transparent(prevents unreadable text when using light mode)
    - Made the "action text" white all the time to prevent readability issues when using light theme(which makes the text black)

--- about.cpp
+++ about.cpp
@@ -5,8 +5,12 @@ About::About(QWidget *parent)
     : QDialog(parent)
     , ui(new Ui::About)
 {
-    this->setFixedSize(434, 152); // Lock current size
+    this->setFixedSize(565, 210); // Lock current window size
     ui->setupUi(this);
+    QString qtVersion = QT_VERSION_STR;
+    ui->license->setText("Qt " + qtVersion+"\nThis application uses the Qt framework © The Qt Company Ltd.\n"
+                                             "Qt is licensed under the GNU Lesser General Public License version "
+                                             "3.\nSee https://www.qt.io/licensing/ for details.");  // Set the license info text
 }
 
 About::~About()
